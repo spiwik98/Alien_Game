8c88d483b69699b0a26ade1acf0988fa65cb4644
spiwik98 <bzalewa@noreply.github.com>
2024-03-23 20:46:50 +0100

    Changes

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings:
     def __init__(self):
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 720
         self.bg_color = (230, 230, 230)
 
         self.ship_limit = 3
